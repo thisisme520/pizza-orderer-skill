1761d58eccd3cea71c60642bcb462880b6108586
thisisme520 <35882069+thisisme520@users.noreply.github.com>
2020-12-19 23:35:09 -0800

    update
    update

--- __init__.py
+++ __init__.py
@@ -1,15 +1,30 @@
-from mycroft import MycroftSkill, intent_file_handler
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill, intent_handler
 
 
-class PizzaOrderer(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
+class KnockKnockSkill(MycroftSkill):
 
-    @intent_file_handler('orderer.pizza.intent')
-    def handle_orderer_pizza(self, message):
-        self.speak_dialog('orderer.pizza')
+    def get_user_response(self, dialog):
+        response = self.get_response(dialog)
+        return response
 
+    @intent_handler(IntentBuilder("").require("knock.knock"))
+    def handle_knock_knock_intent(self, message):
+        # They said "knock knock"
+
+        # You say "who's there?"
+        # Get the next line
+        noun = self.get_user_response("whos.there")
+
+        # They said noun
+        # You say "noun who?"
+        self.speak(noun)
+        self.get_user_response("who")
+
+        # They said a punchline
+        # You laugh in return
+        self.speak_dialog("laugh")
 
 def create_skill():
-    return PizzaOrderer()
+    return KnockKnockSkill()
 
